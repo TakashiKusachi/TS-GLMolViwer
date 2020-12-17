6455d1b6eebfc17e6f66ba656f0e15aafad86a57
TakashiKusachi <aisiars@gmail.com>
2020-12-17 21:09:44 +0900

    update

--- backend_socket_server/startup/startup.py
+++ backend_socket_server/startup/startup.py
@@ -5,9 +5,28 @@ from ase.optimize import BFGS
 from ase.collections import g2
 from ase.build import molecule
 
+import time
+
+from pymysql.err import OperationalError
+
 def up_initial_dataset(db_url):
     db = connect(db_url)
 
+    count = 0
+    while():
+        try:
+            l = len(db)
+            if (l != 0):
+                return
+        except OperationalError as e:
+            count += 1
+            if(count == 3):
+                raise RuntimeError()
+            pass
+        else:
+            break
+
+    count = 0
     for name in g2.names:
         try:
             atoms = molecule(name)
@@ -15,6 +34,12 @@ def up_initial_dataset(db_url):
             db.write(atoms,releaxed=True,name=name,data={'description':'ASE sample data.'})
         except NotImplementedError as e:
             pass
+        except OperationalError as e:
+            count+=1
+            time.sleep(3.0)
+            if(count>3):
+                raise RuntimeError()
+            continue
 
 if __name__=="__main__":
-    startup()
+    up_initial_dataset()
